b96120165d0550f43d81ea8a0c791ad0ca9ed122
pspon <116814650+pspon@users.noreply.github.com>
2025-01-21 21:19:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ filtered_df = combined_df[
     (combined_df['Closing Date Object'] <= pd.to_datetime(end_date)) &
     (combined_df['Salary Min'] >= salary_filter[0]) &
     (combined_df['Salary Max'] <= salary_filter[1]) &
-    ((combined_df['Organization'].str.isin(organization_filter)) | (organization_filter == "All")) &
+    ((combined_df['Organization'].isin(organization_filter)) | (organization_filter == "All")) &
     (combined_df['Location'].str.lower().str.contains(location_filter)) #&
     #(combined_df['Job Title'].str.lower().str.contains(job_filter))
 ]
